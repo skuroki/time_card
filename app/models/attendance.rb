3883f3a3a4c462ad044f5bf85494f4dd61255e1b
Shinsuke KUROKI <shinsukekuroki@gmail.com>
2021-10-03 10:41:37 +0900

    wip

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,12 @@
 class Attendance < ApplicationRecord
   has_one :clock_out, dependent: :destroy
   has_many :rests, dependent: :destroy
+
+  def work_time
+    if clock_out
+      a = (clock_out.finished_at - self.started_at)
+    else
+      nil
+    end
+  end
 end
--- test/system/normals_test.rb
+++ test/system/normals_test.rb
@@ -2,8 +2,13 @@ require "application_system_test_case"
 
 class NormalsTest < ApplicationSystemTestCase
   test "saves daily attendances and rests" do
+    freeze_time
     visit root_url
 
-    assert_selector "h1", text: "Attendances"
+    # click
+    assert_match
+    [1, 2, 3].find do |i|
+      i.to_s
+    end
   end
 end
