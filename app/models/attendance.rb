d5e3f02673dda7d00a0b27243184624f2e0ff1a7
Shinsuke KUROKI <shinsukekuroki@gmail.com>
2021-10-22 18:33:59 +0900

    Attendance always starts at work_date

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,6 +2,17 @@ class Attendance < ApplicationRecord
   has_one :clock_out, dependent: :destroy
   has_many :rests, dependent: :destroy
 
+  before_save do
+    self.started_at ||= Time.zone.local(
+      work_date.year,
+      work_date.month,
+      work_date.day,
+      started_at.hour,
+      started_at.min,
+      started_at.sec
+    )
+  end
+
   def length
     if clock_out
       (clock_out.finished_at - self.started_at) - rests.map(&:length).sum
